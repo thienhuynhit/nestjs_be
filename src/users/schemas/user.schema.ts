5418a082c980606fac28144903001af9a65d0568
thien <thien@techlist.com>
2023-09-22 14:34:01 +0700

    create user schema

--- src/users/schemas/user.schema.ts
+++ src/users/schemas/user.schema.ts
@@ -5,14 +5,29 @@ export type UserDocument = HydratedDocument<User>;
 
 @Schema()
 export class User {
+  @Prop({ required: true })
+  email: string;
+
+  @Prop({ required: true })
+  password: string;
+
   @Prop()
   name: string;
+
+  @Prop()
+  phone: string;
+
+  @Prop()
+  address: string;
 
   @Prop()
   age: number;
 
   @Prop()
-  breed: string;
+  createdAt: Date;
+
+  @Prop()
+  updatedAt: Date;
 }
 
 export const UserSchema = SchemaFactory.createForClass(User);
